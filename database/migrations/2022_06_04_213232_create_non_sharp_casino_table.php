cd625d07d037dfbcbd6d28bef828bb2d8bfd603d
Ben the Beast <happynowbtodd@gmail.com>
2022-06-04 16:41:04 -0500

    allow non-sharp lines

---
+++ database/migrations/2022_06_04_213232_create_non_sharp_casino_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('non_sharp_casinos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('casinoId');
+            $table->string('apiKey');
+            $table->index('casinoId');
+            $table->foreign('casinoId')->references('id')->on('casinos')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sharp_casinos');
+    }
+};
+
